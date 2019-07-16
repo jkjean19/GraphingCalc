af0c3b5943774c6598a844bce0dd978b0313450a
Jaime <47400879+jkjean19@users.noreply.github.com>
2019-07-16 14:25:36 -0400

    Add db and modify image file

--- CalcApp.py
+++ CalcApp.py
@@ -4,9 +4,13 @@ A web application that utilizes the GraphingCalc.py functions.
 """
 
 from flask import Flask, render_template, request
+from urllib.parse import quote
 import app.GraphingCalc as GC
+from app.models import db, Post
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/calc_app'
+db.init_app(app)
 
 @app.route('/')
 def home():
@@ -22,23 +26,35 @@ def result():
     """
     subject = request.form['calc']
     function = request.form['function']
-    file = "./static/images/graph.png"
+    file = './images/' + quote(subject + '&' + function) + '.png'
     
     if subject == 'single':
         result = GC.single_var(function)
         GC.graph_2d(function, file)
+        
+        if not db.session.query(Post).filter(Post.func == function).count():
+            db_post = Post(function, file)
+            db.session.add(db_post)
+            db.session.commit()
+            
         return render_template('results.html',
                                subject = subject,
                                function=function, 
                                first_deriv = result[0],
                                second_deriv = result[1],
                                third_deriv = result[2],
-                               integ = result[-2],
                                integ_sol = result[-1],
-                               url = file)
+                               img_file = file)
+    
     else:
         result = GC.multi_var(function)
         GC.graph_3d(function, file)
+        
+        if not db.session.query(Post).filter(Post.func == function).count():
+            db_post = Post(function, file)
+            db.session.add(db_post)
+            db.session.commit()
+            
         return render_template('results.html', 
                                subject = subject,
                                function=function, 
@@ -47,9 +63,8 @@ def result():
                                partial_XX = result[2],
                                partial_XY = result[3],
                                partial_YY = result[4],
-                               integ = result[-2],
                                integ_sol = result[-1],
-                               url = file)
+                               img_file = file)
 
 if __name__ == "__main__":
     app.run(threaded=True)
