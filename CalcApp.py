33c8379d29c5f587236090a05cb47cff5dcf984d
Jaime <47400879+jkjean19@users.noreply.github.com>
2019-09-05 16:59:43 -0400

    Add try/except to account for errors

--- CalcApp.py
+++ CalcApp.py
@@ -29,40 +29,46 @@ def result():
     file = './images/' + quote(subject + '&' + function) + '.png'
     
     if subject == 'single':
-        result = GC.single_var(function, './static/'+file)
-        
-        if not db.session.query(PostSingle).filter(PostSingle.func == function).count():
-            db_post = PostSingle(function, file)
-            db.session.add(db_post)
-            db.session.commit()
-            
-        return render_template('results.html',
-                               subject = subject,
-                               function=function, 
-                               first_deriv = result[0],
-                               second_deriv = result[1],
-                               third_deriv = result[2],
-                               integ_sol = result[-1],
-                               img_file = file)
-    
+        try:
+            result = GC.single_var(function, './static/'+file)
+
+            if not db.session.query(PostSingle).filter(PostSingle.func == function).count():
+                db_post = PostSingle(function, file)
+                db.session.add(db_post)
+                db.session.commit()
+
+            return render_template('results.html',
+                                   subject = subject,
+                                   function=function, 
+                                   first_deriv = result[0],
+                                   second_deriv = result[1],
+                                   third_deriv = result[2],
+                                   integ_sol = result[-1],
+                                   img_file = file)
+        except:
+            return render_template('index.html')
+
     else:
-        result = GC.multi_var(function, './static/'+file)
-        
-        if not db.session.query(PostMulti).filter(PostMulti.func == function).count():
-            db_post = PostMulti(function, file)
-            db.session.add(db_post)
-            db.session.commit()
-            
-        return render_template('results.html', 
-                               subject = subject,
-                               function=function, 
-                               partial_X = result[0],
-                               partial_Y = result[1],
-                               partial_XX = result[2],
-                               partial_XY = result[3],
-                               partial_YY = result[4],
-                               integ_sol = result[-1],
-                               img_file = file)
+        try:
+            result = GC.multi_var(function, './static/'+file)
+
+            if not db.session.query(PostMulti).filter(PostMulti.func == function).count():
+                db_post = PostMulti(function, file)
+                db.session.add(db_post)
+                db.session.commit()
+
+            return render_template('results.html', 
+                                   subject = subject,
+                                   function=function, 
+                                   partial_X = result[0],
+                                   partial_Y = result[1],
+                                   partial_XX = result[2],
+                                   partial_XY = result[3],
+                                   partial_YY = result[4],
+                                   integ_sol = result[-1],
+                                   img_file = file)
+        except:
+            return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(threaded=True)
